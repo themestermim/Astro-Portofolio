5e700b5626b5533259bc929836d9505dab1820b5
themestermim <themestermim@gmail.com>
2025-01-13 16:41:57 +0330

    working on experiences page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        },
+        container: {
+            padding: '1rem',
+            center: true,
+        },
+        fontFamily: {
+            'archivo': 'Archivo',
+        },
 		extend: {},
 	},
 	plugins: [],
