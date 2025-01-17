b60fc13ddb19d0f2f0e6c692c73a601ce0363a19
Mehran Mahmoudi <themestermim@gmail.com>
2025-01-17 12:13:24 +0330

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://themestermim.github.io/',
-
   integrations: [tailwind()]
 });
