3163ded7b7711e4528a7df5bc899ceb33da4f191
themestermim <themestermim@gmail.com>
2025-01-18 10:15:44 +0330

    skills replaced.

---
+++ src/helpers/api.js
@@ -0,0 +1,242 @@
+export const projectsList = [
+    {
+        id: 1,
+        title: 'Paye1.com',
+        description: 'In my most recent role, I worked on a project called "Paye Yek", which was developed using Nuxt 3 and Tailwind CSS, with state management handled through Pinia. The project was implemented as both an SSR and SPA, and one of the main challenges was accurately filtering advertisements. Through close collaboration with the technical manager and focused research, we achieved successful results. In addition to optimizing performance and SEO, the project was also designed as a PWA (Progressive Web Application), enabling offline use of the application. I learned how to implement app installation on various devices and laid the groundwork for sending push notifications using Pusher JS as the notification provider. Along with performance and SEO optimization, I delved deeper into Service Workers. This project provided an opportunity to thoroughly familiarize myself with the fundamentals and lifecycle of Service Workers. I was able to apply this technology to deliver a smoother and more efficient user experience. This knowledge and experience enabled me to successfully implement push notification services in the project, thereby enhancing the overall user experience.',
+        src: '/assets/projects/paye1Com/fullscreen.webp',
+        logo: '/assets/projects/paye1Com/paye1.png',
+        link: 'https://paye1.com/',
+        techList: [
+            {
+                id: 1,
+                title: 'Vue js',
+                icon: '/assets/skills/vue.svg',
+            },
+            {
+                id: 2,
+                title: 'Nuxt js',
+                icon: '/assets/skills/nuxt.svg',
+            },
+            {
+                id: 3,
+                title: 'Pinia',
+                icon: '/assets/skills/pinia.svg',
+            },
+            {
+                id: 4,
+                title: 'Tailwind CSS',
+                icon: '/assets/skills/tailwind.svg',
+            },
+            {
+                id: 5,
+                title: 'Headless UI',
+                icon: '/assets/skills/headlessui.svg',
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: 'Paye1.org',
+        description: 'This project marked my first experience with WordPress development, where I handled everything from design to implementation. The goal was to create a website builder for automotive companies, enabling them to generate customized designs in the shortest time possible. In this project, we implemented WordPress as a factory and utilized the ACF plugin to explore its extensive capabilities. For styling, we leveraged Sass to enhance the development process. One of the main challenges was learning WordPress development with a programming-focused approach (rather than using tools like Elementor) and working with PHP. For the first time, I designed custom APIs using PHP to manage and process data, which went beyond my previous front-end experience. Although I wasn’t highly skilled at the time, I successfully learned the structure of WordPress back-end development and applied it effectively in this project.',
+        src: '/assets/projects/paye1Org/screen.webp',
+        logo: '/assets/projects/paye1Org/logo.svg',
+        link: 'https://paye1.org/',
+        techList: [
+            {
+                id: 1,
+                title: 'Wordpress',
+                icon: '/assets/skills/wordpress.svg',
+            },
+            {
+                id: 2,
+                title: 'Javascript',
+                icon: '/assets/skills/js.svg',
+            },
+            {
+                id: 3,
+                title: 'PHP',
+                icon: '/assets/skills/php.svg',
+            },
+            {
+                id: 4,
+                title: 'Scss',
+                icon: '/assets/skills/scss.svg',
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: 'Tehran DJ',
+        description: 'This project was my first personal experience where I independently took full responsibility for starting and developing it from scratch to completion. Along the way, I encountered numerous challenges, which I overcame by relying on my research skills and precise error analysis, ultimately completing the project successfully. The project was an e-commerce platform, and the most significant challenges arose in the product filtering and shopping cart management sections. The development began with Nuxt.js version 3, and I used Pinia for state management. For the UI design, I utilized Tailwind CSS and also implemented dark mode functionality. The site was built using SSR (Server-Side Rendering), and currently, the admin panel section is not yet fully deployed. The backend for this project was developed using Laravel.',
+        src: '/assets/projects/tehrandj/light.webp',
+        logo: '/assets/projects/tehrandj/logo.png',
+        link: 'https://front.tehrandj.com/',
+        techList: [
+            {
+                id: 1,
+                title: 'Vue js',
+                icon: '/assets/skills/vue.svg',
+            },
+            {
+                id: 2,
+                title: 'Nuxt js',
+                icon: '/assets/skills/nuxt.svg',
+            },
+            {
+                id: 3,
+                title: 'Pinia',
+                icon: '/assets/skills/pinia.svg',
+            },
+            {
+                id: 4,
+                title: 'Tailwind CSS',
+                icon: '/assets/skills/tailwind.svg',
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: 'Shahr Lavazem Khanegi',
+        description: 'I really love the fascinating "City of Home Appliances" project. It was assigned to me at a time when I didn’t yet have enough expertise in implementing complex designs, but through this project, I got the hang of it. I completed the implementation of the front-end pages, and it was decided that making it dynamic would be handled by the back-end team of the "City of Home Appliances" project, as I was about to leave for military service.',
+        src: '/assets/projects/shahrkhanegi/light.webp',
+        logo: '/assets/projects/shahrkhanegi/logo.png',
+        link: 'https://www.shahrkhanegi.com/',
+        techList: [
+            {
+                id: 1,
+                title: 'Html',
+                icon: '/assets/skills/html.svg',
+            },
+            {
+                id: 2,
+                title: 'Css',
+                icon: '/assets/skills/css.svg',
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: 'Carpet City',
+        description: 'In my portfolio, the "Carpet City" project holds a notable place, where I was responsible for redesigning its homepage while the other pages remained unchanged. The main challenges of this project revolved around implementing the Figma design, which I successfully overcame.',
+        src: '/assets/projects/shahrfarsh/light.webp',
+        logo: '/assets/projects/shahrfarsh/logo.png',
+        link: 'https://shahrfarsh.com/',
+        techList: [
+            {
+                id: 1,
+                title: 'Html',
+                icon: '/assets/skills/html.svg',
+            },
+            {
+                id: 2,
+                title: 'Css',
+                icon: '/assets/skills/css.svg',
+            },
+        ],
+    },
+    {
+        id: 6,
+        title: 'Limoo Shirin',
+        description: 'This website was developed using Nuxt.js version 2, and the main challenge was the implementation of the UI, as the project heavily focused on creating a unique design. I joined the project midway, and we worked as a two-person team to move it forward. Since the codebase was quite intricate, I not only learned how to read and understand someone else\'s code but also how to effectively collaborate with a teammate on the same project.',
+        src: '/assets/projects/limooshirin/light.webp',
+        logo: '/assets/projects/limooshirin/logo.png',
+        link: 'https://www.limooshirin.digital/',
+        techList: [
+            {
+                id: 1,
+                title: 'Vue js',
+                icon: '/assets/skills/vue.svg',
+            },
+            {
+                id: 2,
+                title: 'Css',
+                icon: '/assets/skills/css.svg',
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: 'Arasbdiesel.com',
+        description: 'This project was my first experience with WordPress and was initiated to enhance my skills and strengthen my professional resume. The website was implemented with a mobile-first approach, combining WordPress and Tailwind CSS for optimal performance and responsiveness. Currently, the homepage is fully finalized, and work on completing the remaining pages is ongoing. During this project, I became familiar with the Advanced Custom Fields (ACF) plugin, which significantly streamlined the WordPress development process. The website\'s product section is populated dynamically via an API connected to an internal CRM. To enhance security and prevent unauthorized access, I implemented a proxy between the CRM server and the website, ensuring secure data exchange.',
+        src: '/assets/projects/arasbdiesel/light.webp',
+        logo: '/assets/projects/arasbdiesel/logo.svg',
+        link: 'https://arasbdiesel.com/',
+        techList: [
+            {
+                id: 1,
+                title: 'Wordpress',
+                icon: '/assets/skills/wordpress.svg',
+            },
+            {
+                id: 2,
+                title: 'Javascript',
+                icon: '/assets/skills/js.svg',
+            },
+            {
+                id: 3,
+                title: 'PHP',
+                icon: '/assets/skills/php.svg',
+            },
+            {
+                id: 4,
+                title: 'Tailwind CSS',
+                icon: '/assets/skills/tailwind.svg',
+            },
+        ],
+    },
+    {
+        id: 8,
+        title: 'React JS Sample',
+        description: 'This is my personal website, developed using React and Tailwind CSS, with features such as dark mode and multilingual support. My primary focus was on optimizing the user interface, particularly the shopping cart section, especially the product addition process. Additionally, a simple music player was designed and implemented on the site. This project served as a personal challenge to evaluate and improve my skills in React and Redux.',
+        src: '/assets/projects/mehrandev/light.webp',
+        logo: '/assets/projects/mehrandev/logo.svg',
+        link: 'https://mehrandev.liara.run/',
+        techList: [
+            {
+                id: 1,
+                title: 'React JS',
+                icon: '/assets/skills/react.svg',
+            },
+            {
+                id: 2,
+                title: 'Redux',
+                icon: '/assets/skills/redux.svg',
+            },
+            {
+                id: 3,
+                title: 'Tailwind CSS',
+                icon: '/assets/skills/tailwind.svg',
+            },
+        ],
+    },
+    {
+        id: 9,
+        title: 'Nuxt JS Sample',
+        description: 'This is my personal online website, developed using Nuxt.js, Tailwind CSS, and Pinia. The goal of this website is to provide an interactive and user-friendly online resume and showcase completed projects. The main focus of this project has been on the user interface (UI), and currently, the data is being loaded statically. The website also supports features such as dark mode and multilingual capabilities, offering a better user experience for a diverse audience. Additionally, I am learning back-end development and API creation to dynamically retrieve and manage the site\'s data through APIs. This project is still under development and continuously being improved.',
+        src: '/assets/projects/mestermimVercel/screen.webp',
+        logo: '/assets/projects/mestermimVercel/logo.png',
+        link: 'https://mestermim.vercel.app/',
+        techList: [
+            {
+                id: 1,
+                title: 'Nuxt JS',
+                icon: '/assets/skills/nuxt.svg',
+            },
+            {
+                id: 2,
+                title: 'Pinia',
+                icon: '/assets/skills/pinia.svg',
+            },
+            {
+                id: 3,
+                title: 'AOS JS',
+                icon: '/assets/skills/aos.png',
+            },
+            {
+                id: 3,
+                title: 'Tailwind CSS',
+                icon: '/assets/skills/tailwind.svg',
+            },
+        ],
+    },
+]
